e9cb624bfd7c00a70ab54f32d27cdb64e9542bf4
Heyladyspace <70000677+Heyladyspace@users.noreply.github.com>
2020-09-03 14:23:39 +1200

    database created and classlibary type change
    created dtabase
    created InventoryItem table
    created RoomItem table.
    recreated classlibrary to C# and deleted the vb classlibrary

---
+++ Abyss/AbyssDatabase/RoomItem.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[RoomItem]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ Abyss/AbyssLibrary/player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class player
+{
+	inventoryItem activeitem;
+
+	public player()
+	{
+		
+	}
+}
