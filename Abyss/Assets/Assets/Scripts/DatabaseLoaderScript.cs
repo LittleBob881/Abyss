95800cfc192520199df29a0c948bd82c34d2b7ea
Heyladyspace <70000677+Heyladyspace@users.noreply.github.com>
2020-09-18 12:19:22 +1200

    deleted errror thingy

--- Abyss/Assets/Assets/Scripts/DatabaseLoaderScript.cs
+++ Abyss/Assets/Assets/Scripts/DatabaseLoaderScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class DatabaseLoaderScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+
+
+    public void NewGame()
     {
+       //load roomItem here
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void ConGame()
     {
-        
+        //code stuff here 
     }
 }
