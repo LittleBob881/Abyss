916bcf37bddb2c0acf9e600cc0a11f7899b93699
JadhaMACK01 <jadhamack@gmail.com>
2020-09-25 00:01:45 +1200

    Working Doors.
    Doors in all rooms with controllers

---
+++ Abyss/Assets/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DoorScript : MonoBehaviour
+{
+    private bool showPopup = false;
+    private AssetBundle loadedAssets;
+    private string[] scenes;
+    public GameObject doorButton;
+    public string scene;
+
+    void Start()
+    {
+        loadedAssets = AssetBundle.LoadFromFile("C:\\users\\kiwik\\OneDrive\\Documents\\GitHub\\Abyss\\Abyss\\Assets\\Scenes");
+        scenes = loadedAssets.GetAllScenePaths();
+
+        Button next = doorButton.GetComponent<Button>();
+        next.onClick.AddListener(loadscreen);
+    }
+
+    public void loadscreen() 
+    {
+        Debug.Log("Scene loading: " + scene);
+        SceneManager.LoadScene(scene);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision");
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Find("doorButton").GetComponent<Button>().interactable = true;
+            Debug.Log("Entering Collision");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("LeavingCollision");
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Find("doorButton").GetComponent<Button>().interactable = false;
+            Debug.Log("Entering Collision");
+        }
+    }
+}
