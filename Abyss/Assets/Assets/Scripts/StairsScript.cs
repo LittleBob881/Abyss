bd61afd0a79b369fb3b128028a0e9ccfc166d5da
JadhaMACK01 <jadhamack@gmail.com>
2020-09-25 04:49:38 +1200

    Creation of UpperHallway/ DoorTesting

--- Abyss/Assets/Assets/Scripts/StairsScript.cs
+++ Abyss/Assets/Assets/Scripts/StairsScript.cs
@@ -6,38 +6,13 @@ using UnityEngine.SceneManagement;
 public class StairsScript : MonoBehaviour
 {
     private bool showPopup = false;
-    // Start is called before the first frame update
-
-    private AssetBundle loadedAssets;
-    private string[] scenes;
     public bool isInRange;
 
     void Start()
     {
-        loadedAssets = AssetBundle.LoadFromFile("C:\\users\\kiwik\\OneDrive\\Documents\\GitHub\\Abyss\\Abyss\\Assets\\Scenes");
-        scenes = loadedAssets.GetAllScenePaths();
+        
     }
 
-    void Update()
-    {
-        if (isInRange) { 
-            if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
-            {
-                Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                RaycastHit raycastHit;
-                if (Physics.Raycast(raycast, out raycastHit))
-                {
-                    Debug.Log("Something Hit");
-                    if (raycastHit.collider.name == "Box Collider 2D")
-                    {
-                        Debug.Log("Soccer Ball clicked");
-                    }
-                }
-            }
-        }
-    }
-
-    
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Collision");
@@ -73,12 +48,12 @@ public class StairsScript : MonoBehaviour
 
     void ShowGUI(int windowID)
     {
-        GUI.Label(new Rect(65, 40, 200, 30), "Would you like to go up or down?");
-
-        if (GUI.Button(new Rect(50, 150, 75, 30), "Up"))
+        
+            GUI.Label(new Rect(65, 40, 200, 30), "Would you like to go up or down?");
+            if (GUI.Button(new Rect(50, 150, 75, 30), "Up"))
         {
             Debug.Log("Scene loading: " + "Top Floor");
-            SceneManager.LoadScene("hallway_Top");
+            SceneManager.LoadScene("UpperHallway");
         }
 
         if (GUI.Button(new Rect(150, 150, 75, 30), "Down"))
@@ -86,6 +61,5 @@ public class StairsScript : MonoBehaviour
             Debug.Log("Scene loading: " + "Bottom Floor");
             SceneManager.LoadScene("hallway_Bottom");
         }
-
     }
 }
---
+++ Abyss/Assets/Assets/Tests/DoorsTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class DoorsTest
+    {
+        // Tests loading three different Scenes within hallway
+        
+        [Test]
+        public void DoorsTestSimplePasses()
+        {
+            //Creating the shit
+            Debug.Log("Loading first scene.");
+            EditorSceneManager.OpenScene("Assets/Scenes/Hallway.unity");
+            Debug.Log("Loading this dude.");
+            string KitchenDoor = "KitchenDoor";
+            // Use the Assert class to test conditions
+            Assert.IsTrue(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Hallway"));
+            Debug.Log("Dude bro is loaded and ready to go baybe");
+           var door = GameObject.Find(KitchenDoor);
+            Assert.IsNotNull(door, "Missing Button" + KitchenDoor);
+            Debug.Log("Button is here.");
+
+            //Set as selected
+            EventSystem.current.SetSelectedGameObject(door);
+            door.GetComponent<Button>().onClick.Invoke();
+            Debug.Log("Button click");
+            Assert.IsTrue(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Kitchen"));
+            Debug.Log("U changed!!");
+        }
+
+    }
+}
