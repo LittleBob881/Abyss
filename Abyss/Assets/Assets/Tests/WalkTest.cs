c33d8971d8230c53e268251d1a83bb224e09dd48
iCheeksi <68730051+iCheeksi@users.noreply.github.com>
2020-09-25 13:59:48 +1200

    Shelbys walkTest not done but its there

--- Abyss/Assets/Assets/Tests/WalkTest.cs
+++ Abyss/Assets/Assets/Tests/WalkTest.cs
@@ -13,16 +13,34 @@ namespace Tests
         public void WalkTestSimplePasses()
         {
             // Use the Assert class to test conditions
-        }
+            var walk = new Walk();
+            var left = -1056;
+            var right = 1056;
+            Sprite character = Resources.Load("main_sheet1");
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator WalkTestWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            int i = 0;
+            while (i < Input.touchCount)
+            {
+                if (Input.GetTouch(i).position.x > ScreenWidth / 2)
+                {
+                    //Walking Right
+                    walkCharacter(right);
+                }
+                if (Input.GetTouch(i).position.x < ScreenWidth / 2)
+                {
+                    //Walking left
+                    walkCharacter(left);
+                }
+                ++i;
+            }
+
+            //Act
+            var walkingSide = walk.walkCharacter();
+
+
+            //Assert
+            Assert.That(walk, ISet.EqualTo(left));
         }
     }
+
 }
