c42bbdb4a53e7750a561c1b1714aca5e6f5ba28a
iCheeksi <68730051+iCheeksi@users.noreply.github.com>
2020-10-06 17:19:18 +1300

    Updated sceneboundary

--- Abyss/Assets/Assets/Scripts/SceneBoundary.cs
+++ Abyss/Assets/Assets/Scripts/SceneBoundary.cs
@@ -8,6 +8,6 @@ public class SceneBoundary : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 70f, 100f), transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 68f, 101f), transform.position.y, transform.position.z);
     }
 }
