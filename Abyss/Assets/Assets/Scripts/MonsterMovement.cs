b3ca1c59d8024f1b1c193c9d90369beea2857803
LittleBob881 <68731508+LittleBob881@users.noreply.github.com>
2020-10-08 16:04:53 +1300

    Monster randomly walking
    Monster randomly walks within room

--- Abyss/Assets/Assets/Scripts/MonsterMovement.cs
+++ Abyss/Assets/Assets/Scripts/MonsterMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class MonsterMovement : MonoBehaviour
 {
@@ -9,7 +10,14 @@ public class MonsterMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector3 localScale;
-    private bool facingR = false;
+    private bool facingR;
+
+    //Variables for randomized walking
+    private float floorPosition;
+    private float wallLeft;
+    private float wallRight;
+    private float doorPosition;
+    private System.Random rand = new System.Random();
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +25,34 @@ public class MonsterMovement : MonoBehaviour
         localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
         direction = -22f;
+        facingR = false;
+        wallLeft = 70f;
+        wallRight = 100f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < 70f)
-            direction = 22f;
-        else if (transform.position.x > 100f)
-            direction = -22f;
+        walk();
+    }
+
+    void walk()
+    {
+        if(!(transform.position.x < wallRight && transform.position.x > wallLeft))
+        {
+            direction *= -1;
+            localScale.x *= -1;
+        }
+        else
+        {
+            int randomGen = rand.Next(1, 100);
+            if (randomGen == 1)
+            {
+                direction *= -1;
+                localScale.x *= -1;
+            }
+        }
+        transform.localScale = localScale;
     }
 
     void FixedUpdate()
@@ -33,21 +60,21 @@ public class MonsterMovement : MonoBehaviour
         rb.velocity = new Vector2(direction * movement, rb.velocity.y);
     }
 
-    void LateUpdate()
-    {
-        facing();
-    }
+    //void LateUpdate()
+    //{
+    //    facing();
+    //}
 
-    void facing()
-    {
-        if (direction > 0)
-            facingR = true;
-        else if (direction < 0)
-            facingR = false;
+    //void facing()
+    //{
+    //    if (direction > 0)
+    //        facingR = true;
+    //    else if (direction < 0)
+    //        facingR = false;
 
-        if ((facingR && localScale.x < 0) || (!facingR && localScale.x > 0));
-        localScale.x *= -1;
+    //    if ((facingR && localScale.x < 0) || (!facingR && localScale.x > 0));
+    //    localScale.x *= -1;
 
-        transform.localScale = localScale;
-    }
+    //    transform.localScale = localScale;
+    //}
 }
