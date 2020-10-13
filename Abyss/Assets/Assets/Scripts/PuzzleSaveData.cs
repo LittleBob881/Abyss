03c8247a26b878d105e9156ab156f8254b640d9f
LittleBob881 <68731508+LittleBob881@users.noreply.github.com>
2020-10-13 14:27:28 +1300

    Finished Draft of Save

---
+++ Abyss/Assets/Assets/Scripts/PuzzleSaveData.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class PuzzleSaveData
+{
+    public int puzzleItemProgress;
+    public int puzzleProgress;
+    public Boolean[] unlocks;
+
+    public PuzzleSaveData(PuzzleScript puzzle)
+    {
+        this.puzzleItemProgress = puzzle.GetPuzzleItemValue();
+        this.puzzleProgress = puzzle.GetPuzzleValue();
+        this.unlocks = puzzle.GetUnlock();
+    }
+}
