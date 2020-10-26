131437d07345564f1e90903e2ec4286dd4b5e314
iCheeksi <68730051+iCheeksi@users.noreply.github.com>
2020-10-26 21:28:25 +1300

    Update Walk Script

--- Abyss/Assets/Assets/Scripts/Walk.cs
+++ Abyss/Assets/Assets/Scripts/Walk.cs
@@ -7,9 +7,9 @@ public class Walk : MonoBehaviour
 {
     private Rigidbody2D rigid;
     private float speed;
-    private AudioSource audioSrc;
     private float walkSound = 2f;
     public bool alive = true;
+    private AudioSource audioSrc;
     public DeathScript DeathScript;
     public Vector3 respawnPoint;
 
@@ -21,11 +21,15 @@ public class Walk : MonoBehaviour
 
     private void Update()
     {
+        //This will play the walking sound, stops when the player is idle and plays sound when the player is on the move
         audioSrc = GetComponent<AudioSource>();
+        //Updates scale x of the character depending on which side the character is facing (Left -, right +)
         Vector3 characterScale = transform.localScale;
+        //Takes in touch count from Android device (up to 5 fingers touch)
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
+            //Switch case to make the character walk either left or right. Play sound as the player is walking and update the scale x
             switch (touch.phase)
             {
                 case TouchPhase.Began:
@@ -64,13 +68,9 @@ public class Walk : MonoBehaviour
         DeathScript.PlayerDeath();
     }
 
+    //Collider used with IsTrigger to save Mirror in safe room as respawn Point
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Monster")
-        {
-            //
-        }
-
         if (collision.tag == "LoadingPoint")
         {
             respawnPoint = collision.transform.position;
