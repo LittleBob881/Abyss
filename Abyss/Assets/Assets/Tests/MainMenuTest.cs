c97bbd37a1b53fbe1841fc97aa0c6e93085bbcd0
iCheeksi <68730051+iCheeksi@users.noreply.github.com>
2020-10-26 18:28:20 +1300

    Test

---
+++ Abyss/Assets/Assets/Tests/MainMenuTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace Tests
+{
+    public class MainMenuTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MainMenuTestSimplePasses()
+        {
+            //ARRANGE
+            var main = EditorSceneManager.OpenScene("Assets/Scenes/titlemenu/MainMenu");
+            var WorldScene = EditorSceneManager.OpenScene("Assets/Scenes/World");
+
+
+            //Adding next scene 
+            EditorSceneManager.LoadScene(main.buildIndex + 1);
+
+
+            //ASSERT
+            Assert.That(main, Is.EqualTo(WorldScene));
+
+        }
+    }
+}
